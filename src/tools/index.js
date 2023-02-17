1ed68f8377ab15a1b30c75fa3d09c9e742064f58
unknown <degenshadow@proton.me>
2023-02-17 19:47:00 +1100

    first commit

---
+++ src/tools/index.js
@@ -0,0 +1,21 @@
+import jobad from './jobad';
+import summarize from './summarize';
+import codeInterpret from './interpret';
+import intro from './intro';
+import helloworld from './helloworld';
+import example from './example';
+import marketing from './Marketing ';
+
+const TOOLS = [
+
+	codeInterpret,
+	intro,
+	jobad,
+	summarize,
+	helloworld,
+	example,
+	marketing,
+
+]
+
+export default TOOLS
